71063d2f1d3b223857d65c2d465985c208c505af
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2023-09-01 19:56:53 +0300

    Update SuggestionsAPI.java

--- src/main/java/io/github/aratakileo/suggestionsapi/SuggestionsAPI.java
+++ src/main/java/io/github/aratakileo/suggestionsapi/SuggestionsAPI.java
@@ -3,25 +3,58 @@ package io.github.aratakileo.suggestionsapi;
 import io.github.aratakileo.suggestionsapi.suggestion.*;
 import io.github.aratakileo.suggestionsapi.core.SuggestionsProcessor;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
 
 public class SuggestionsAPI implements ClientModInitializer {
     private final static HashMap<String, Suggestion> suggestions = new HashMap<>();
     private final static ArrayList<Injector> injectors = new ArrayList<>();
+    private final static ArrayList<Supplier<@NotNull List<Suggestion>>> resourceDependedInjectors = new ArrayList<>();
 
     private static HashMap<String, Suggestion> dynamicSuggestions;
+    private static boolean isResourcesLoaded = false;
 
     @Override
     public void onInitializeClient() {
+        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(
+                new SimpleSynchronousResourceReloadListener() {
+                    @Override
+                    public ResourceLocation getFabricId() {
+                        return new ResourceLocation("suggestionsapi", "");
+                    }
 
+                    @Override
+                    public void onResourceManagerReload(ResourceManager resourceManager) {
+                        if (isResourcesLoaded) return;
+
+                        isResourcesLoaded = true;
+
+                        resourceDependedInjectors.forEach(
+                                injector -> injector.get().forEach(SuggestionsAPI::addSuggestion)
+                        );
+                        resourceDependedInjectors.clear();
+                    }
+                }
+        );
     }
 
     public static void addSuggestion(@NotNull Suggestion suggestion) {
         suggestions.put(suggestion.getSuggestionText(), suggestion);
+    }
+
+    public static void registerResourceDependedInjector(@NotNull Supplier<@NotNull List<Suggestion>> injector) {
+        resourceDependedInjectors.add(injector);
     }
 
     public static void removeSuggestion(@NotNull Suggestion suggestion) {
