308ec3def35b01a87f07bdfc8bebe63f1a49f6f5
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-01-29 22:32:54 +0300

    Neoforge port

---
+++ neoforge/src/main/java/io.github.aratakileo.suggestionsapi.neoforge/SuggestionsApiNeoforge.java
@@ -0,0 +1,10 @@
+package io.github.aratakileo.suggestionsapi.neoforge;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("suggestionsapi")
+@OnlyIn(Dist.CLIENT)
+public class SuggestionsApiNeoforge {
+}
